47ecf8ae184a62d62b99a06c05c7cf75a689af0a
gusev6056-design <gusev6056@gmail.com>
2026-06-02 18:05:38 +0300

    Create cards.py

---
+++ cards.py
@@ -0,0 +1,26 @@
+# Фиктивный файл cards.py для обхода импорта
+# Все функции создания карточек временно отключены
+
+def create_profile_card(*args, **kwargs):
+    return None
+
+def create_map_card(*args, **kwargs):
+    return None
+
+def create_top_card(*args, **kwargs):
+    return None
+
+def create_shop_card(*args, **kwargs):
+    return None
+
+def create_inventory_card(*args, **kwargs):
+    return None
+
+def create_lobby_card(*args, **kwargs):
+    return None
+
+def create_match_start_card(*args, **kwargs):
+    return None
+
+def create_match_result_card(*args, **kwargs):
+    return None
